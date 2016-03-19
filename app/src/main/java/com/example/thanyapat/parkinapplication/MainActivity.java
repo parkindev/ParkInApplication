1be38314d09c30aafc3a45bc3e21b7e1d2858462
Thanyapat <thanyapat.kosi@gmail.com>
2016-03-19 17:35:21 +0700

    initial commit

--- app/src/main/java/com/example/thanyapat/parkinapplication/MainActivity.java
+++ app/src/main/java/com/example/thanyapat/parkinapplication/MainActivity.java
@@ -362,15 +362,7 @@ public class MainActivity extends AppCompatActivity
         googleApiClient.connect();
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.thanyapat.parkinapplication/http/host/path")
+        Action viewAction = Action.newAction(Action.TYPE_VIEW, "Main Page", Uri.parse("http://host/path"), Uri.parse("android-app://com.example.thanyapat.parkinapplication/http/host/path")
         );
         AppIndex.AppIndexApi.start(googleApiClient, viewAction);
     }
@@ -381,15 +373,7 @@ public class MainActivity extends AppCompatActivity
         super.onStop();
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.thanyapat.parkinapplication/http/host/path")
+        Action viewAction = Action.newAction( Action.TYPE_VIEW,"Main Page", Uri.parse("http://host/path"), Uri.parse("android-app://com.example.thanyapat.parkinapplication/http/host/path")
         );
         AppIndex.AppIndexApi.end(googleApiClient, viewAction);
         if (googleApiClient != null && googleApiClient.isConnected()) {
@@ -473,7 +457,7 @@ public class MainActivity extends AppCompatActivity
                             JSONObject data = picture.getJSONObject("data");
 
                             if(data.getBoolean("is_silhouette")){
-                                // TODO: set a silhouette image instead
+                                profile = BitmapFactory.decodeResource(getResources(), R.drawable.blank_profile_pic);
                             }else{
                                 String pictureUrl = "https://graph.facebook.com/" + response.getJSONObject().getString("id") + "/picture?type=large";
 
